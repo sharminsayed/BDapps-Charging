60f901dcb65ae7013c55e9a71b46e3bc23739bc2
sharminsayed <sharminsayedshammi@gmail.com>
2020-08-28 17:04:10 +0600

    BDapps charging library v0.1.0

--- app/src/main/java/com/sharmin/bdapps/MainActivity.java
+++ app/src/main/java/com/sharmin/bdapps/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.sharmin.charging.AdsLib;
+import com.sharmin.charging.SP;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize adslib
+        AdsLib adsLib=ChargingInstance.getAdsLib();
+        //Checking Subscription Status
+        adsLib.checkSubStatus(SP.getSubCode());
+        //Subscribe for this application
+        adsLib.subscribe();
     }
 }
